c3344cd16f8ebfd7f1608b446c15b0d36b197417
ANDRES DAVID VILLAMIZAR PATIÑO <AVILLAMIZAR@net-uno.net.ve>
2024-11-13 14:39:25 -0400

    commitmend

---
+++ app/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>xdd</Text>
+      <Button color={'red'} title={'ola'} onPress={()=> alert('ola')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
