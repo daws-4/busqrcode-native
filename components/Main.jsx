fec777f376a5fd1a05dc3d10b56d3b7589210d95
daws-4 <villamizarandresdavid@gmail.com>
2025-02-03 14:40:21 -0400

    03-02-2025

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import { View, Text, Pressable, Button } from "react-native";
+import { View, Text, Pressable,  ActivityIndicator, ScrollView } from "react-native";
 import {
   useUserContext,
   useBusIdContext,
@@ -29,10 +29,13 @@ export function Main() {
   const [selectedTime, setSelectedTime] = useState(new Date());
   const [selectedRealTime, setSelectedRealTime] = useState(new Date());
   const [showTimePicker, setShowTimePicker] = useState(false);
+  const [getRegistros, setGetRegistros] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
  
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true); // Iniciar carga
       try {
         const data = (await axios.get(`https://stllbusqrcode.vercel.app/api/app/rutas`)).data;
         let rutasl = [];
@@ -44,12 +47,24 @@ export function Main() {
           }
         }
         const buses = (await axios.get(`https://stllbusqrcode.vercel.app/api/app/unidades`)).data;
-        setBusList(buses);
-        setRutas(rutasl);
+   
+            const response  = (await axios.post(`https://stllbusqrcode.vercel.app/api/app/timestamp/fiscal`, {
+                numero_fiscal: user.numero,
+                id_fiscal: user._id,
+            }));
+              const sortedData = response.data.sort((a, b) => new Date(b.timestamp_salida) - new Date(a.timestamp_salida));
+
+
+            setGetRegistros(sortedData);
+            setBusList(buses);
+            setRutas(rutasl);
       } catch (error) {
         console.log(error + " error");
+     } finally {
+        setIsLoading(false); // Finalizar carga
       }
     };
+
     fetchData();
   }, []);
   const handleSubmit = async () => {
@@ -91,17 +106,6 @@ export function Main() {
     }
 
   }
-  const selectedItem = {
-    title: "Selected item title",
-    description: "Secondary long descriptive text ...",
-  };
-  const advanceTimeByFourHours = (selectedTime) => {
-    const newTime = new Date(selectedTime);
-    newTime.setHours(newTime.getHours() + 4);
-    console.log(newTime)
-    console.log(`Hora adelantada: ${newTime}`);
-    return(newTime);
-  };
   
   const onTimeChange = (event, selectedDate) => {
     const currentDate = selectedDate || selectedTime;
@@ -126,6 +130,7 @@ export function Main() {
     };
 
   return (
+    <ScrollView>
     <Screen>
       <View className="flex flex-col items-center justify-center">
         <Text className="text-2xl">Bienvenido Fiscal {user.numero}</Text>
@@ -156,7 +161,7 @@ export function Main() {
             >
                <Picker.Item key='r._id' label='Selecciona una Ruta' value={null} />
               {rutas.map((r) => (
-              <Picker.Item key={r._id} label={r.nombre} value={r._id} />
+                <Picker.Item key={r._id} label={r.nombre} value={r._id} />
               ))}
             </Picker>
           </View>
@@ -165,10 +170,10 @@ export function Main() {
         </Pressable>
         {showTimePicker && (
           <DateTimePicker
-            value={selectedTime}
-            mode="time"
-            display="default"
-             onChange={onTimeChange}
+          value={selectedTime}
+          mode="time"
+          display="default"
+          onChange={onTimeChange}
           />
         )}
 
@@ -186,6 +191,40 @@ export function Main() {
               </Pressable>}
         </View>
       )}
+
+
+         <View className='m-6'>
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          getRegistros.length > 0 ? (
+            <View className="mt-6 p-4">
+              {getRegistros.map((registro) => {
+                const bus = busList.find(bus => bus._id === registro.id_unidad);
+                return (
+                  <View className="m-4 p-4 bg-slate-200 rounded" key={registro._id}>
+                    <Text className="text-black text-black/90 mb-2 mx-4 text-lg">
+                      <Text className="font-bold text-black">Control: </Text>
+                      {bus ? bus.numero : 'N/A'}
+                    </Text>
+                    <Text className="text-black text-black/90 mb-2 mx-4 text-lg">
+                      <Text className="font-bold text-black">Hora de salida: </Text>
+                      {formatDate1(registro.timestamp_salida)}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
+          ) : (
+            <Text>No hay registros disponibles</Text>
+          )
+        )}
+      </View>
+
+
     </Screen>
+  </ScrollView>
   );
+
+  
 }
